ede81de26f4dae8be52777bf85a319f7f10fd0bd
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-07-20 13:05:18 -0300

    .

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,18 +1,52 @@
-import CartWidget from "../CartWidgets/CartWidget"
+import React from "react";
+import CartWidget from "../CartWidgets/CartWidget";
 
 const NavBar = () => {
     return (
-        <nav>
-            <h3>Cloud Store</h3>
-            <div>
-                <button>Buzos</button>
-                <button>Remeras</button>
-                <button>Joggings</button>
-                <button>Accesorios</button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="/">
+                    Cloud Store
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <button className="nav-link btn btn-link" type="button">
+                                Buzos
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link btn btn-link" type="button">
+                                Remeras
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link btn btn-link" type="button">
+                                Joggings
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link btn btn-link" type="button">
+                                Accesorios
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <CartWidget />
             </div>
-            <CartWidget />
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
