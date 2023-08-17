3d16f8e449822034f7c61987ff0b82be2ba0987e
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-08-17 14:48:32 -0300

    .

--- src/componentes/Catalog/Catalog.js
+++ src/componentes/Catalog/Catalog.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Catalog.css';
+import './Catalog.css'; // Importa los estilos
 
 const Catalog = ({ products, addToCart }) => {
   return (
     <div className="catalog">
-      <h2>Productos</h2>
+      <h2>Catálogo de Productos</h2>
       <div className="product-list">
         {products.map((product) => (
           <div key={product.id} className="product">
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>Precio: ${product.price}</p>
+            <p>{product.details}</p> {/* Agrega los detalles del producto */}
             <button onClick={() => addToCart(product)}>Agregar al carrito</button>
           </div>
         ))}
