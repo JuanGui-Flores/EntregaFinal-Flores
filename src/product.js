7d50f3494414f51afda0ff18101ee29051921136
JuanGui-Flores <juanguiflroes2002@gmail.com>
2023-09-12 21:21:51 -0300

    .

--- src/product.js
+++
@@ -1,7 +0,0 @@
-const products = [
-    { id: 1, name: 'Buzo Azul', description: 'Un cómodo buzo azul.', price: 29.99 },
-    { id: 2, name: 'Remera Estampada', description: 'Una remera con diseño único.', price: 19.99 },
-  ];
-  
-  export default products;
-  
